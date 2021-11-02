eb9c180d7517b4d23566efddf6052cd0b959c08d
Gildo Neto <gildoneto@hotmail.com>
2021-11-02 02:25:50 -0300

    Adiciona exercicio 01

---
+++ exercicios/ex_001.js
@@ -0,0 +1,85 @@
+// EXERCÍCIO 001
+
+// DESAFIO DE EXEMPLO
+// 00 - Escreva uma expressão matemática que some os valores 5(number) e 2(number). 
+// 00.1 - EXTRA: qual o resultado desta instrução?
+// resposta 00.1 -> 7
+5 + 2
+
+// 01 - Declare uma variável com o identificador 'var1' sem inicializá-la. (sem atribuir nenhum valor)
+// 01.1 - Qual o tipo deste valor? Lembre sempre de usar o DevTools do Chrome para fazer seus testes.
+// resposta 01.1 -> 
+// DICA: typeof nomeDaVariavel
+
+
+// 02 - Declare uma variável com o identificador 'num' e atribua um valor do tipo Number.
+
+
+// 03 - Declare uma variável com o identificador 'soma' e atribua a ela a soma dos valores 6(number) e 7(number).
+
+
+// 04 - Declare uma variável com o identificador 'sub' e atribua a ela a subtração dos valores 4(number) e 9(number).
+
+
+// 05 - Declare uma variável com o identificador 'saudacao' e atribua a ela o valor Olá (string).
+
+
+// 06 - Declare uma variável com o identificador 'nome' e atribua a ela o valor [Seu Nome](string).
+
+
+// 07 - Declare uma variável com o identificador 'concat' e atribua a ela a concatenação entre as variáveis saudacao e nome.
+
+
+// 08 - Declare uma variável com o identificador 'mult' e atribua a ela a multiplicação dos valores 3(number) e 5(number).
+
+
+// 09 - Declare uma variável com o identificador 'div' e atribua a ela a divisão dos valores 50(number) e 10(number).
+
+
+// 10 - Declare uma variável com o identificador 'resto1' e atribua a ela o resto da divisão entre os valores 10(number) e 2(number).
+
+
+// 11 - Declare uma variável com o identificador 'incremento' e atribua a ela o valor 4(number).
+// Na linha abaixo use o operador de incremento nesta mesma variável
+
+// 12 - Declare uma variável com o identificador 'decremento' e atribua a ela o valor 100(number).
+// Na linha abaixo use o operador de decremento nesta mesma variável.
+
+// 13 - Declare uma variável com o identificador 'logic1' e atribua a ela o valor VERDADEIRO(boolean) em Javascript.
+
+// 14 - Declare uma variável com o identificador 'logic2' e atribua a ela o valor FALSO(boolean) em Javascript.
+
+// 15 - Declare uma variável com o identificador 'logic3' que receba uma operação booleana que compare os valores das variáveis
+// soma e sub.
+
+// 16 - Declare uma variável com o identificador 'logic4' que receba uma operação booleana que compare os valores da variável
+// mult e o valor numérico 15.
+
+// 17 - Diminua -3 do valor atual da variável soma já declarada, usando o operador de atribuição de subtração "-="
+
+// 18 - Adicione +8 do valor atual da variável sub já declarada, usando o operador de atribuição de soma "+="
+
+// 19 - Divida por 2 o valor atual da variável mult já declarada, usando o operador de atribuição de divisão "/="
+
+// 20 - Multiplique por 3 o valor atual da variável sub já declarada, usando o operador de atribuição de multiplicação "*="
+
+
+
+module.exports = {
+    var1,
+    num,
+    soma,
+    sub,
+    saudacao,
+    nome,
+    concat,
+    mult,
+    div,
+    resto1,
+    incremento,
+    decremento,
+    logic1,
+    logic2,
+    logic3,
+    logic4
+};
