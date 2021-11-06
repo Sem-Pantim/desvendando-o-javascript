abf923b7d06634ce7ddd478f3faa9a7ca1053986
Gildo Neto <gildoneto@hotmail.com>
2021-11-06 19:58:59 -0300

    Atualiza exercicio 001

--- exercicios/ex001/ex001.js
+++ exercicios/ex001/ex001.js
@@ -1,6 +1,6 @@
 // EXERCÍCIO 001
 
-// DESAFIO DE EXEMPLO
+// EXEMPLO
 // 00 - Escreva uma expressão matemática que some os valores 5(number) e 2(number). 
 // 00.1 - EXTRA: qual o resultado desta instrução?
 // resposta 00.1 -> 7
@@ -73,23 +73,107 @@
 
 // 20 - Multiplique por 3 o valor atual da variável sub já declarada, usando o operador de atribuição de multiplicação "*="
 
+/* 
+=============================================================
+    PARA QUE O TESTE FUNCIONE, NÃO ALTERE O CÓDIGO ABAIXO! =D
+=============================================================
+*/
 
+function rodarTestes(){
 
-module.exports = {
-    var1,
-    num,
-    soma,
-    sub,
-    saudacao,
-    nome,
-    concat,
-    mult,
-    div,
-    resto1,
-    incremento,
-    decremento,
-    logic1,
-    logic2,
-    logic3,
-    logic4
-};
+    const successText = "RESPOSTA CORRETA"
+    const errorText = "TENTE OUTRA VEZ"
+    
+    const printSuccess = (id) => document.getElementById(`${id}`).innerHTML = `${successText}`;
+    const printError = (id) => document.getElementById(`${id}`).innerHTML = `${errorText}`;
+    
+    const expect = (result, id) => ({
+        toBe: (expected) => { 
+            if (result === expected){
+                printSuccess(id)
+            } else {
+                printError(id)
+            }
+        },
+    
+        notToBe: (expected, id) => { 
+            if (result !== expected){
+                printSuccess(id)
+            } else {
+                printError(id)
+            }
+        }
+    })
+    
+    const it = (id, description, result, callback) => {
+        //console.log('---', description, result)
+        console.log(`${id}`)
+        document.getElementById(`${id}`).innerHTML = `${description + result}`;
+        callback()
+    }
+    
+    it ('s01', 'EX 01 - "var1" É UNDEFINED? -> ', typeof var1, () => {
+        expect(typeof var1, 'e01').toBe('undefined')
+    })
+    
+    it ('s02', 'EX 02 - O TIPO DA VARIÁVEL "num" É number? -> ', typeof num, () => {
+        expect(typeof num, 'e02').toBe('number')
+    })
+    
+    it ('s05', 'EX 05 - A VARIÁVEL "saudacao" É "Olá"? -> ', saudacao, () => {
+        expect(saudacao, 'e05').toBe('Olá')
+    })
+    
+    it ('s06', 'EX 06 - O TIPO DA VARIÁVEL "nome" É string? -> ', typeof nome, () => {
+        expect(typeof nome, 'e06').toBe('string')
+    })
+    
+    it ('s07', 'EX 07 - O TIPO DA VARIÁVEL "concat" É string? -> ', typeof concat, () => {
+        expect(typeof concat, 'e07').toBe('string')
+    })
+    
+    it ('s08', 'EX 08 - A VARIÁVEL "mult" É 7.5? -> ', mult, () => {
+        expect(mult, 'e08').toBe(7.5)
+    })
+    
+    it ('s09', 'EX 09 - A VARIÁVEL "div" É 5? -> ', div, () => {
+        expect(div, 'e09').toBe(5)
+    })
+    
+    it ('s10' ,'EX 10 - A VARIÁVEL "resto1" É 0? -> ', resto1, () => {
+        expect(resto1, 'e10').toBe(0)
+    })
+    
+    it ('s11','EX 11 - A VARIÁVEL "incremento" É 6? -> ', incremento, () => {
+        expect(incremento, 'e11').toBe(6)
+    })
+    
+    it ('s12', 'EX 12 - A VARIÁVEL "decremento" É 99? -> ', decremento, () => {
+        expect(decremento, 'e12').toBe(99)
+    })
+    
+    it ('s13', 'EX 13 - A VARIÁVEL "logic1" É true? -> ', logic1, () => {
+        expect(logic1, 'e13').toBe(true)
+    })
+    
+    it ('s14', 'EX 14 - A VARIÁVEL "logic2" É false? -> ', logic2, () => {
+        expect(logic2, 'e14').toBe(false)
+    })
+    
+    it ('s15', 'EX 15 - A VARIÁVEL "logic3" É false? -> ', logic3, () => {
+        expect(logic3, 'e15').toBe(false)
+    })
+    
+    it ('s16', 'EX 16 - A VARIÁVEL "logic4" É true? -> ', logic4, () => {
+        expect(logic4, 'e16').toBe(true)
+    })
+    
+    it ('s17', 'EX 17 - A VARIÁVEL "soma" É 10? -> ', soma, () => {
+        expect(soma, 'e17').toBe(10)
+    })
+    
+    it ('s18','EX 18 - A VARIÁVEL "sub" É 9? -> ', sub, () => {
+        expect(sub, 'e18').toBe(9)
+    })
+    
+    }
